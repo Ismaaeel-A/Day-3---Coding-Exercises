ab7ff47c93f7eebec8bb7cd6b8fe81a797973678
Ismaaeel-A <ismaaeelahmed42@gmail.com>
2024-05-17 09:28:50 +0200

    completed

---
+++ script/code.js
@@ -0,0 +1,14 @@
+let stringWord = "boss"
+console.log(typeof stringWord) //returns string
+
+stringWord = 7
+console.log(typeof stringWord) //returns number
+
+let container = [1, 2, 3, 4, 5, 6, 7 ]
+console.log(typeof container)
+
+let question = true
+console.log(typeof question)
+
+let mystery 
+console.log(typeof mystery)
